8e20554d9ddfbaddca74956ab21b94766219f9c3
PS2Comrade <Oliullah321123@protonmail.com>
2024-10-14 19:56:21 +0600

    Update background.js

--- background.js
+++ background.js
@@ -1,27 +1,10 @@
 browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "updateManifest") {
     const manifest = browser.runtime.getManifest();
-    manifest.chrome_settings_overrides.search_provider.search_url = request.searchUrl;
-    manifest.chrome_settings_overrides.search_provider.suggest_url = request.suggestUrl;
+    manifest.chrome_settings_overrides.search_provider.search_url = request.searchUrl || "https://search.demoniak.ch/search?q={searchTerms}";
+    manifest.chrome_settings_overrides.search_provider.suggest_url = request.suggestUrl || "https://search.demoniak.ch/autocompleter?q={searchTerms}";
 
     // Update the manifest.json file
     browser.runtime.sendMessage({ action: "updateManifestFile", manifest });
-  }
-});
-
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.action === "updateManifestFile") {
-    const manifest = request.manifest;
-    const json = JSON.stringify(manifest, null, 2);
-    const blob = new Blob([json], { type: "application/json" });
-    const url = URL.createObjectURL(blob);
-
-    // Update the manifest.json file
-    browser.runtime.getBackgroundPage().fetch(url)
-      .then(response => response.text())
-      .then(text => {
-        const manifestFile = browser.runtime.getManifestFile();
-        manifestFile.write(text);
-      });
   }
 });
